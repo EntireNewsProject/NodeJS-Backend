9037a990ef29b6503d21a71da90897153f770bf5
khushaljobanputra <khushal2611@gmail.com>
2017-09-26 21:08:52 -0700

    Set the value for max limit and corrected the errors. This version of the code should run without any errors.

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 var api = require('./routes/api');
 
 var app = express();
--- routes/api.js
+++ routes/api.js
@@ -1,11 +1,11 @@
+var moduleNews = require('../models/news');
 var express = require('express');
 var router = express.Router();
-var moduleNews = require('../models/news');
-
+var MAX_LIMIT = 12;
 
 moduleNews.News.find().sort({createdAt: -1})
     .limit(MAX_LIMIT)
-    .select(FIELDS)
+    //.select(FIELDS)
     .exec()
     .then(function (result) {
         if(!result) res.sendStatus(500);

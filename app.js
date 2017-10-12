ef729f2cf82dc3a876e9bb814b09c73f9f22bd59
khushaljobanputra <khushal2611@gmail.com>
2017-10-12 16:55:41 -0700

    Error solving

--- app.js
+++ app.js
@@ -37,14 +37,14 @@ app.use(function (err, req, res, next) {
 });
 
 // error handler
-app.use(function (err, req, res) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error');
-});
+// app.use(function (err, req, res) {
+//     // set locals, only providing error in development
+//     res.locals.message = err.message;
+//     res.locals.error = req.app.get('env') === 'development' ? err : {};
+//
+//     // render the error page
+//     res.status(err.status || 500);
+//     res.render('error');
+// });
 
 module.exports = app;

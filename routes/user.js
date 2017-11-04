bb6a9f1bf65a64fe59c2493e13bf83df1a36e2c4
07alexander <07alexander@gmail.com>
2017-11-03 20:44:13 -0700

    updated routes for clarity between each route

--- routes/user.js
+++ routes/user.js
@@ -10,7 +10,91 @@ var app = express();
 mongoose.Promise = promise;
 
 router.route('/user')
-    .get(function (req, res, next) {  //me
+    //Login route
+    .post(function(req, res){
+        if (req.body.email && req.body.password) {
+            var params = {};
+            params.email = req.body.email;
+            params.password = req.body.password;
+            params.active = true;
+
+            var user = moduleUser.User(params);
+
+            user.findOne({ //look for user and give token
+                username: req.body.username
+            }, function (err, user) {
+                if(!user) {
+                    res.json({success: false, message: 'User Not Found.'});
+                }
+                else {
+                    if(user.password !== req.body.password){
+                        res.json({ success: false, message:'Wrong Password'});
+                    }
+                    else{
+                        var token = 'JWT '+ jwt.sign(user, app.get('secretOrKey'),{
+                            expiresInMinutes: 10080 //expires in 7 days
+                        });
+
+                        res.json({
+                            success: true,
+                            message: 'Token successful',
+                            token: token
+                        });
+                    }
+                }
+
+
+            })
+            /*user.load(function (err) {
+                if (err)
+                    res.status(400).json({
+                        error: 'Internal server error'
+                    });
+                else
+                    res.status(201).json({
+                        msg: 'Login successful'
+                    });
+            });*/
+        }
+        else{
+            res.status(401).json({
+                error: 'All information not provided'
+            });
+        }
+    })
+
+    //Register route
+    .post(function(req, res){
+        if (req.body.email && req.body.password){
+            var params = {};
+            params.email = req.body.email;
+            params.password = req.body.password;
+            params.active = true;
+
+            if (req.body.email) params.email = req.body.email;
+            if (req.body.password) params.password = req.body.password;
+            //TODO: Check for unique username and email
+
+            var user = new moduleUser.User(params);
+            user.save(function (err) {
+                if (err)
+                    res.status(400).json({
+                        error: 'Internal server error'
+                    });
+                else
+                    res.status(201).json({
+                        msg: 'User created successfully'
+                    });
+            });
+        }
+        else{
+            res.status(401).json({
+                error: 'All information not provided'
+            });
+        }
+    })
+    //ME route
+    .get(function (req, res, next) {
         var username = req.body.username;
 
         //route middleware to verify token
@@ -48,86 +132,6 @@ router.route('/user')
         else {
             res.status(404).json({
                 error: 'Token not found'
-            });
-        }
-    })
-    .post(function(req, res){ // register
-        if (req.body.email && req.body.password){
-            var params = {};
-            params.email = req.body.email;
-            params.password = req.body.password;
-            params.active = true;
-
-            if (req.body.email) params.email = req.body.email;
-            if (req.body.password) params.password = req.body.password;
-            //TODO: Check for unique username and email
-
-            var user = new moduleUser.User(params);
-            user.save(function (err) {
-                if (err)
-                    res.status(400).json({
-                        error: 'Internal server error'
-                    });
-                else
-                    res.status(201).json({
-                        msg: 'User created successfully'
-                    });
-            });
-        }
-        else{
-            res.status(401).json({
-                error: 'All information not provided'
-            });
-        }
-    })
-    .post(function(req, res){ // login
-        if (req.body.email && req.body.password) {
-            var params = {};
-            params.email = req.body.email;
-            params.password = req.body.password;
-            params.active = true;
-
-            var user = moduleUser.User(params);
-
-            user.findOne({ //look for user and give token
-                username: req.body.username
-            }, function (err, user) {
-                if(!user) {
-                    res.json({success: false, message: 'User Not Found.'});
-                }
-                else {
-                    if(user.password !== req.body.password){
-                        res.json({ success: false, message:'Wrong Password'});
-                    }
-                    else{
-                        var token = 'JWT '+ jwt.sign(user, app.get('secretOrKey'),{
-                            expiresInMinutes: 10080 //expires in 7 days
-                        });
-
-                        res.json({
-                            success: true,
-                            message: 'Token successful',
-                            token: token
-                        });
-                    }
-            }
-
-
-            })
-            /*user.load(function (err) {
-                if (err)
-                    res.status(400).json({
-                        error: 'Internal server error'
-                    });
-                else
-                    res.status(201).json({
-                        msg: 'Login successful'
-                    });
-            });*/
-        }
-        else{
-            res.status(401).json({
-                error: 'All information not provided'
             });
         }
     })

66b0599908c6c20864cf058b0162d67fc3655546
07alexander <07alexander@gmail.com>
2017-11-06 12:27:53 -0800

    added unique param for email user schema

--- models/user.js
+++ models/user.js
@@ -5,7 +5,7 @@ var Schema = mongoose.Schema;
 
 var userSchema = new Schema({
     username: {type: String, required: true, trim: true, unique: true},
-    email: {type: String, required: true, trim: true},
+    email: {type: String, required: true, trim: true, unique: true},
     password: {type: String, required: true},
     type: {type: Number, default: 0},
     fullName: {type: String, trim: true},

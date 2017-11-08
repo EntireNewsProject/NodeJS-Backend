8aa06f2b3f44c3e981ccdbdd747fe6a41e2b68e6
khushaljobanputra <khushal2611@gmail.com>
2017-11-07 16:51:08 -0800

    Error fixed

--- routes/api.js
+++ routes/api.js
@@ -126,8 +126,7 @@ router.route('/news/:id')
 
 router.route('/news/:id/save')
     .get(function(req, res) {
-        var id = req.param.id;
-        console.log(id);
+        var id = req.params.id;
         if (id) {
             moduleNews.News
                 .findOneAndUpdate({_id: id}, {$inc: {saves: 1}}, {new: true})

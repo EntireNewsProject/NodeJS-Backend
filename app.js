43f0f5196f0317d5e34db54292e749ea31f24ae1
khushaljobanputra <khushal2611@gmail.com>
2017-11-08 22:01:07 -0800

    Commented out link to user api

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api', api);
-app.use('/user', user);
+// app.use('/user', user);
 
 // catch 404 and forward to error handler
 app.use(function (err, req, res) {

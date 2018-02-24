d9576e3268547ecad4f01d887d74c9f442269be4
Shifat Denocyte <thunderroid@gmail.com>
2018-02-24 15:50:04 -0800

    Basic recommendation system structure

--- models/recommendations.js
+++ models/recommendations.js
@@ -4,53 +4,37 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const likeSchema = new Schema({
-    userId: {
-        type: Schema.Types.ObjectId,
-        ref: 'User',
-        required: true
-    },
-    newsId: {
-        type: Schema.Types.ObjectId,
-        ref: 'News',
-        required: true
-    }
-}, {
-    _id: false,
-    timestamps: true
-});
+    userId: {type: Schema.Types.ObjectId, ref: 'User', required: true},
+    newsId: {type: Schema.Types.ObjectId, ref: 'News', required: true}
+}, {_id: false, timestamps: true});
 
 const viewSchema = new Schema({
-    userId: {
-        type: Schema.Types.ObjectId,
-        ref: 'User',
-        required: true
-    },
-    newsId: {
-        type: Schema.Types.ObjectId,
-        ref: 'News',
-        required: true
-    }
-}, {
-    _id: false,
-    timestamps: true
-});
+    userId: {type: Schema.Types.ObjectId, ref: 'User', required: true},
+    newsId: {type: Schema.Types.ObjectId, ref: 'News', required: true}
+}, {_id: false, timestamps: true});
+
+const similarSchema = new Schema(
+
+);
+
+const suggestionSchema = new Schema(
+
+);
 
 // noinspection JSCheckFunctionSignatures
 likeSchema.index({
     userId: 1,
     newsId: 1
-}, {
-    unique: true
-});
+}, {unique: true});
 // noinspection JSCheckFunctionSignatures
 viewSchema.index({
     userId: 1,
     newsId: 1
-}, {
-    unique: true
-});
+}, {unique: true});
 
 module.exports = {
     Likes: mongoose.model('Likes', likeSchema),
-    Views: mongoose.model('Views', viewSchema)
+    Views: mongoose.model('Views', viewSchema),
+    Similars: mongoose.model('Similars', similarSchema),
+    Suggestions: mongoose.model('Suggestions', suggestionSchema)
 };

a33a33c4f990d8adcc5a5f2189bf0c36ce5680cd
khushaljobanputra <khushal2611@gmail.com>
2018-03-10 19:02:51 -0800

    difference function added

--- config/tools.js
+++ config/tools.js
@@ -1,10 +1,38 @@
-const pluck = (arr, key) => arr.map(obj => obj[key]);
+class ArrayTools {
+    pluck = (arr, key) => arr.map(obj => obj[key]);
 
-const flatten = arr => arr.reduce((flat, next) => flat.concat(next), []);
+    flatten = arr => arr.reduce((flat, next) => flat.concat(next), []);
 
-const without = (arr, ...args) => {
-    for (let a of args) {
-        let index = arr.indexOf(a);
-        arr.splice(index, 1);
+    without = (arr, ...args) => {
+        for (let a of args) {
+            let index = arr.indexOf(a);
+            arr.splice(index, 1);
+        }
+    };
+
+    unique = arr => arr.filter((elem, pos, arr1) => arr1.indexOf(elem) == pos);
+
+    //this difference function is different that other functions here as it returns the result instead of storing it in the parameters provided.
+    difference = (arr1, arr2) => {
+        let temp = [];
+        for (let i in arr1) {
+            if(!arr2.includes(arr1[i])) temp.push(arr1[i]);
+        }
+        for(i in arr2) {
+            if(!arr1.includes(arr2[i])) temp.push(arr2[i]);
+        }
+        temp.sort((a,b) => a-b);
+        return temp;
+    };
+
+    findWhere = (obj, data) => {
+
+    };
+
+    intersection = (arr1, arr2) => {
+
     }
+}
+module.exports = {
+    ArrayTools
 };

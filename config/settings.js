62c842d75e261d245656664a063c693cb9d4a71f
khushaljobanputra <khushal2611@gmail.com>
2018-04-29 14:51:55 -0700

    server updated

--- config/settings.js
+++ config/settings.js
@@ -4,5 +4,5 @@ module.exports = {
     jwtSession: {session: false},
     //database: 'mongodb://EntireNewsApp:Js3dgK5sdg7BHJIsdGH89JdKsd9fB0@127.0.0.1:27569/entirenews?authSource=entirenews',
     //database: 'mongodb://127.0.0.1:27017/entirenews'
-    database: 'mongodb://EntireNewsApp:Js3dgK5sdg7BHJIsdGH89JdKsd9fB0@ec2-52-26-183-246.us-west-2.compute.amazonaws.com:27569/entirenews?authSource=entirenews'
+    database: 'mongodb://EntireNewsApp:Js3dgK5sdg7BHJIsdGH89JdKsd9fB0@ec2-18-216-30-82.us-east-2.compute.amazonaws.com:27569/entirenews?authSource=entirenews'
 };

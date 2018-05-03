a1f73a81a967925f3609289cff293705e12dce49
studentperson <31675305+studentperson@users.noreply.github.com>
2018-05-03 08:33:42 -0700

    These are the tests so far, still trying to figure out how to get the thing to work Installed jest and supertest added "test": "jest" to the scripts in the package.json

---
+++ tests/news.test.js
@@ -0,0 +1,9 @@
+const news = require('../routes/news');
+
+test('tests something...', () => {
+    //const PORT = 3000;
+    //const log = jest.fn();
+    //user(log, PORT);
+    //expect(log.mock.calls).toHaveLength(1);
+    //expect(log.mock.calls[0][0]).toBe(`Example app listening on port ${PORT.toString()}!`);
+});
